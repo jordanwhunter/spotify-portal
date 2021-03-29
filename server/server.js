ad79db68fed28aaa584bc45c4cff0b4a60056f55
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-29 17:56:47 -0400

    Required dotenv

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const spotifyApi = require('spotify-web-api-node');
+const spotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -8,4 +10,21 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true, useNewUrlParser: true }));
 app.use(express.json());
 
+app.post('/login', (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new spotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: '24c06c22abe549bb9dcc9c8f40a0096a',
+    clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET
+  });
 
+  spotifyApi.authorizationCodeGrant(code).then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in
+    })
+  }).catch(() => {
+    res.sendStatus(400)
+  })
+});
