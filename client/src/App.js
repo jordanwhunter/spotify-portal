fd72b7754a1ea202c1f5f5c0d0f8424abe284f8f
jordanwhunter <jordanwhunter.mail@gmail.com>
2021-03-29 17:57:29 -0400

    Imported Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Login from './components/authentication/Login';
+import Dashboard from './components/main/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const code = new URLSearchParams(window.location.search).get('code');
+
 function App() {
-  return (
-    <Login />
-  );
+  return code
+    ? <Dashboard code={code} />
+    : <Login />
 }
 
 export default App;
